4c221d4a96ca23cebfccd34a9a2c180cd56e60d2
unknown <atong321.as@gmail.com>
2023-08-03 16:41:34 +0700

    hehehehe

--- app/Http/Controllers/Api/RatingController.php
+++ app/Http/Controllers/Api/RatingController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\ProductService;
 use Illuminate\Http\Request;
 
 class RatingController extends Controller
 {
-    //
+    public function store(Request $request, ProductService $service, String $id){
+        $rating = $service->addRating($id,$request->all());
+        return response($rating, 201);
+    }
 }
--- database/migrations/2023_08_03_093635_alter_rating.php
+++ database/migrations/2023_08_03_093635_alter_rating.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ratings', function (Blueprint $table) {
-            $table->id();
+        Schema::table('ratings', function (Blueprint $table) {
+        
+            $table->dropColumn('count');
             $table->double('rating');
-            $table->timestamps();
+
         });
     }
 
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ratings');
-    }
+        //
+    }x
 };
