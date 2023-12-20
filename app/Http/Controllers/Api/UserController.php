6ce4b0710d1901ae7ae1d274ee2fd1531bca5bdf
StevenG0h <atong321.as@gmail.com>
2023-12-20 20:56:20 +0700

    hghjklgl

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Admin;
 use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+	if($request->user() != null){
+            $user = Admin::where('user_id',$request->user()->id)->with(['role'])->first();
+            if($user->role->permission == 0){
+                $user = User::with(['admins.role','admins.unitUsaha'])->where('id',$user->user_id)->whereRelation('admins','isActive',1)->orderBy('updated_at','desc')->paginate(25);
+                return response($user,200);
+            }
+        }
         $user = User::with(['admins.role','admins.unitUsaha'])->whereRelation('admins','isActive',1)->orderBy('updated_at','desc')->paginate(25);
         return response($user,200);
     }
